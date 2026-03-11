cd814f0a44fa40f924e138ddaa17fc90b408dcb4
ruel <ruel.almonia@ntt.com.ph>
2026-03-11 18:00:10 +0800

    Updated the setup for mysql and docker connection, also install and used the DBeaver for my local database.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Necessary for Docker
+    port: 5175,
+    strictPort: true,
+    hmr: {
+      host: 'localhost', // Allows your browser to connect to the dev server
+    },
+    watch: {
+      usePolling: true, // Fixes file-sync issues on many Linux/WSL systems
+    },
+  },
 })
